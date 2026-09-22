2b1c69dd04e566980ad71a7c6c53f27bff74bb06
crst11 <cristian02morales@gmail.com>
2026-09-22 15:33:10 -0500

    feat(config): environments de DEV, PRE y PROD SCRUM-43

---
+++ src/environments/environment.pre.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  nombreAmbiente: 'PRE',
+  // Se completa cuando quede decidido el hosting de preproducción (ADR pendiente).
+  apiUrl: 'https://PENDIENTE-DECIDIR-HOSTING-PRE/api',
+  googleClientId: '',
+  vapidPublica: '',
+};
